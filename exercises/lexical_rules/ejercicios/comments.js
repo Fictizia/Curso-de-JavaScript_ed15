7237aaff8425b5af2cbc1ba569a0086b4f36d457
PIKERUS\pablo <pablogarpi@gmail.com>
2020-10-02 12:12:14 +0200

    ejercicios comentarios y variables

---
+++ exercises/lexical_rules/ejercicios/comments.js
@@ -0,0 +1,8 @@
+/* Este es un comentario de línea */
+
+/* 
+Este 
+es un
+comentario
+de bloque
+*/
---
+++ exercises/lexical_rules/ejercicios/variables.js
@@ -0,0 +1,19 @@
+//Creamos variable
+let x;
+x = 1;
+
+//Otra variable - array -
+let a = [1, 3, 5, 7];
+
+//Creamos constante
+const y = "Adios mundo";
+
+
+// Inicializamos variable
+console.log(x);
+
+// Inicializamos la otra variable
+console.log(a);
+
+// Inicializamos constante
+console.log(y);
