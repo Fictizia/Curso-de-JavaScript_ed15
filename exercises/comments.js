2865ee4bd9ae5f529bef128c6b67fb0aa6eb4db3
Fernando <euriloco@gmail.com>
2020-10-02 09:06:36 +0200

    Primeros ejercicios

---
+++ exercises/comments.js
@@ -0,0 +1,6 @@
+// Este es un comentario de línea
+
+/* Este es un comentario de bloque 
+    que ocupa varias líneas.
+    Saludos.
+*/
---
+++ exercises/variables.js
@@ -0,0 +1,5 @@
+let precioRebajado;     // Declaración de la variable
+precioRebajado = 22.3; // Inicialización de la variable
+
+const cuadrado;        // Declaración de una constante
+cuadrado = x => x**2;  // Inicialización de la constante
